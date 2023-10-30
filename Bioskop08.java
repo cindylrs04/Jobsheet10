ea08fe283302ec820837bba96981ee67704c333e
cindylrs04 <larasaticindy231@gmail.com>
2023-10-30 12:09:30 +0700

    Bioskop08 Modifikasi No 5

--- Bioskop08.java
+++ Bioskop08.java
@@ -18,8 +18,8 @@ public class Bioskop08 {
 
         System.out.println(penonton.length);
 
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: " + barisPenonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang garis ke-" + (i+1) + ": " + penonton[i].length);
             
         }
     }
